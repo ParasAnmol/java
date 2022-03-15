3784044fcc1711e545e701603411d1fcd97e3ade
ParasAnmol <parasanmol16@gmail.com>
2022-03-15 15:30:32 -0400

    another approach

--- getting started/gcdandlcmofanumber.java
+++ getting started/gcdandlcmofanumber.java
@@ -6,12 +6,12 @@ public class Main{
     int b = scn.nextInt();
     int t1 = a;
     int t2 = b;
-    while(t2 != 0){
+    while(t1%t2 != 0){
       int t3 = t1 % t2;
       t1 = t2;
       t2 = t3;
     }
-    System.out.println(t1);
-    System.out.println((a*b)/t1);
+    System.out.println(t2);
+    System.out.println((a*b)/t2);
   }
 }
