1b3b77af5c12c7f59b69ef9dca9632cdc715c60b
ParasAnmol <parasanmol16@gmail.com>
2022-03-15 13:46:38 -0400

    Reverse a number

---
+++ reverseanumber.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main{
+  public static void main(String[] args){
+    Scanner scn  = new Scanner(System.in);
+    int num = scn.nextInt();
+    while(num != 0){
+      int i = num % 10;
+      num = num / 10;
+      System.out.println(i);
+    }
+  }
+}
