ae281e8114c0eb88297d2bee1c99462159efe4f6
ParasAnmol <parasanmol16@gmail.com>
2022-03-18 15:45:51 -0400

    broken economy

---
+++ ArrayandString/brokeneconomy.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+public static void be(int[] a, int k){
+    int st = 0, ed = a.length-1;
+    int ceil = 0, floor = 0;
+    while(st <= ed){
+        int mid = (st + ed)/2;
+        if(a[mid] < k){
+            st = mid + 1;
+            floor = a[mid];
+        }
+        else if(a[mid] > k){
+            ed = mid - 1;
+            ceil = a[mid];
+        }
+        else{
+            System.out.println(a[mid]);  
+            return;
+        }
+    }
+    System.out.println(ceil);
+    System.out.println(floor);
+}
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int a[] = new int[n];
+    for (int i = 0; i <= a.length-1; i++) {
+        a[i] = scn.nextInt();
+    }
+    int k = scn.nextInt();
+    be(a,k);
+ }
+
+}
