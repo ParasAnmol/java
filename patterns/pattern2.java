c199dd664c9173f139f2feff092ef3623d453934
ParasAnmol <parasanmol16@gmail.com>
2022-03-21 09:08:00 -0400

    Pattern 2 other method

--- patterns/pattern2.java
+++ patterns/pattern2.java
@@ -14,3 +14,23 @@ public class Main {
         }
     }
 }
+
+                                or
+
+                                import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        int  n = scn.nextInt();
+        for (int  i = 1 ; i <= n ; i++){
+            for(int  j = n ; j >= i ; j--){
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+    }
+}
